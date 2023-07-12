4ae49d22d6e4909dc11688e03cc68ff3427f281a
JoaoAHaupt <joao.haupt@aluno.ifsp.edu.br>
2023-07-11 22:41:39 -0300

    commitamdo

---
+++ src/mozartGifs.py
@@ -0,0 +1,13 @@
+gifs = {
+    "1" : "https://media.giphy.com/media/mGj3SVN7xbPQ4/giphy.gif",
+    "2" : "https://media.giphy.com/media/kcgTmYoQwz92wWrDBd/giphy.gif",
+    "3" : "https://media.giphy.com/media/2HZdc4ApduJ2g/giphy.gif",
+    "4" : "https://media.giphy.com/media/l0HU6IqQcTEbzeDmM/giphy.gif",
+    "5" : "https://media.giphy.com/media/6i4pwFrt58a9xBcWVa/giphy.gif",
+    "6" : "https://media.giphy.com/media/3o75272MSGHbzazaXC/giphy.gif",
+    "7" : "https://media.giphy.com/media/VcvlXneJImIg21BuzT/giphy.gif",
+    "8" : "https://media.giphy.com/media/iJn9OqrWAIQI472FAJ/giphy.gif",
+    "9" : "https://media.giphy.com/media/jtLbWkeIjfaCliYBPx/giphy.gif",
+    "10" : "https://media.giphy.com/media/ZDPSNMP6k9hfy/giphy.gif"
+
+}
